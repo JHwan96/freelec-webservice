82a8a1fb47aa03149c44cf9817b236059420a8cb
JHwan96 <dlwnghks00@gmail.com>
2023-11-23 21:03:22 +0900

    feat: 구글 로그인 추가, 세션 설정 추가
    - jdbc로 세션 설정
    - OAuth2를 통한 구글 로그인 추가 (다시 학습해봐야함)

--- src/main/java/book/springboot/web/IndexController.java
+++ src/main/java/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package book.springboot.web;
 
+import book.springboot.domain.config.auth.LoginUser;
+import book.springboot.domain.config.auth.dto.SessionUser;
 import book.springboot.service.posts.PostsService;
 import book.springboot.web.dto.PostsResponseDto;
 import book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,13 +11,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
+
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
     @GetMapping("/posts/save")
